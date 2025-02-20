873f3a85e262f24cf8e4144580555f07048ea314
Redsoul1308 <gabriella.dc1308@gmail.com>
2025-02-20 12:43:53 +0100

    aggiunta pacchetti

---
+++ Models/Utente.cs
@@ -0,0 +1,7 @@
+public class Utente
+{
+    public int UtenteID { get; set; }
+    public string? NomeUtente { get; set; }
+    public string? Email { get; set; }
+    public DateTime DataRegistrazione { get; set; }
+}
---
+++ Models/Viaggio.cs
@@ -0,0 +1,13 @@
+/*public class Viaggio
+{
+    public int ViaggioID { get; set; }
+    public int UtenteID { get; set; }
+    public string Titolo { get; set; }
+    public string Descrizione { get; set; }
+    public DateTime DataInizio { get; set; }
+    public DateTime DataFine { get; set; }
+    public string Località { get; set; }
+
+    public Utente Utente { get; set; }
+}*/
+
